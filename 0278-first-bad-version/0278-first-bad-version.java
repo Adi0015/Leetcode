d54e8e2bc9ecf69bf4ba02103207b6de2fa94231
Aditya Gohil <89626455+Adi0015@users.noreply.github.com>
2024-02-12 08:06:03 +0530

    Time: 25 ms (73.31%), Space: 40.3 MB (51.01%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -8,11 +8,12 @@ public class Solution extends VersionControl {
         
         while (start <= end ){
             int mid = start +  (end-start )/2 ;
-            boolean status = isBadVersion(mid);
-            if (status == true && isBadVersion(mid - 1) == false){return mid ;} 
-            if (status == true ){end = mid -1 ;}
-            else{ start = mid +1 ;}
+            if(!isBadVersion(mid)){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
         }
-        return -1;
+        return start;
     }
 }
